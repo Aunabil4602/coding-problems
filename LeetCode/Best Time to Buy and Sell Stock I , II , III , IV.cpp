fac6c16fe7814cdef84120ff21e386aacecc3498
Aunabil4602 <aunabilac@gmail.com>
2020-04-14 14:20:49 +0600

    Update and rename Best Time to Buy and Sell Stock I , II , III.cpp to Best Time to Buy and Sell Stock I , II , III , IV.cpp

--- LeetCode/Best Time to Buy and Sell Stock I , II , III , IV.cpp
+++ LeetCode/Best Time to Buy and Sell Stock I , II , III , IV.cpp
@@ -1,4 +1,4 @@
-////////////////////////////// solution for :II
+////////////////////////////// solution for :II , complexity O (n)
 
 class Solution {
 public:
@@ -12,7 +12,7 @@ public:
     }
 };
 
-//////////////////////////// solution for :I
+//////////////////////////// solution for :I  , complexity O (n)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -26,8 +26,7 @@ public:
     }
 };
 
-/////////////////////////// solution for :III
-
+/////////////////////////// solution for :III , complexity O (n)
 
 class Solution {
 public:
@@ -41,7 +40,7 @@ public:
             if(i>0)dp[i]=max(dp[i],dp[i-1]);
             
         }
-        
+       
         int mx=0;
         int ans=0;
         int cost=0;
@@ -52,5 +51,29 @@ public:
             ans=max(ans,cost);
         }
         return ans;
+    }
+};
+
+ /////////////////////////////// solution for:IV, complexity O(k*n)
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int dp[prices.size()+2];
+        int old[prices.size()+2];
+        memset(dp,0,sizeof(dp));
+        memset(old,0,sizeof(old));
+        int n=prices.size();
+        k=min(k,n/2+1);
+        while(k--){
+            int best=-100000;
+            for(int i=1;i<=n;i++){
+                dp[i]=max(dp[i],dp[i-1]);
+                dp[i]=max(dp[i],prices[i-1]+best);
+                best=max(best,old[i-1]-prices[i-1]);
+            }
+            for(int i=1;i<=n;i++)old[i]=dp[i];
+        }
+        return dp[n];
     }
 };
