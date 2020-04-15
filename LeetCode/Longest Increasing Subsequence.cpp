f75b89610a1c6063e15fd2d35dffe97e8689109e
Aunabil4602 <aunabilac@gmail.com>
2020-04-15 14:13:42 +0600

    Update Longest Increasing Subsequence.cpp

--- LeetCode/Longest Increasing Subsequence.cpp
+++ LeetCode/Longest Increasing Subsequence.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if(nums.size()==0)return 0;
         vector<int> dp;
-        dp.push_back(nums[0]);
         for(auto x:nums){
             int pos=lower_bound(dp.begin(),dp.end(),x)-dp.begin();
             if(pos<dp.size())dp[pos]=x;
